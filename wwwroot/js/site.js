591d521364c56f36087fd0781fd223297aa18787
Daria Danilova <d.d.danilova2001@gmail.com>
2023-12-03 13:55:58 +0500

    created product page

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,17 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+var img = document.getElementsByClassName("productPhoto")[0];
+var descrip = document.getElementsByClassName("description")[0];
+var container = document.getElementsByClassName("containerPhoto")[0];
+img.onmouseover = function (event) {
+    img.style.opacity = "0.6";
+    container.style.background = "#0000";
+    descrip.style.display = "block";
+};
+
+img.onmouseout = function (event) {
+    img.style.opacity = "1";
+    container.style.background = "";
+    descrip.style.display = "none";
+};
