d6e24415f7832e3c797fb2a7f71f1c5d5f995e00
Daria Danilova <d.d.danilova2001@gmail.com>
2023-12-05 19:42:29 +0500

    Адаптация сайта под мобильные устройства

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,6 +1,14 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+/*navbar - collapse collapse d - sm - inline - flex justify - content - between*/
+var menu = document.getElementsByClassName("menu")[0];
+menu.onclick = function (event) {
+    var header = document.getElementsByClassName("collapse")[0];
+    console.log(header);
+    header.style.display = "block";
+}
+
 var img = document.getElementsByClassName("productPhoto")[0];
 var descrip = document.getElementsByClassName("description")[0];
 var container = document.getElementsByClassName("containerPhoto")[0];
@@ -15,3 +23,4 @@ img.onmouseout = function (event) {
     container.style.background = "";
     descrip.style.display = "none";
 };
+
